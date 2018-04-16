af942787966a4af134c4f5834b8b962a3afdf8e5
songzongyao <704140897@qq.com>
2018-04-17 05:49:06 +0800

    first commit

---
+++ src/main/java/com/lab_admin/lab_admin/LabAdminApplication.java
@@ -0,0 +1,12 @@
+package com.lab_admin.lab_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabAdminApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lab_admin/lab_admin/LabAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lab_admin.lab_admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
